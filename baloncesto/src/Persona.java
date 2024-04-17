29fad80c2f0809c8d3bbcf702bd9ff4582d05aa3
ItsGuerrero <jguerrerobelmonte@gmail.com>
2024-04-17 13:56:22 +0200

    Baloncesto

---
+++ baloncesto/src/Persona.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Medac
+ */
+public class Persona {
+    String nombre;
+    int puntuacion;
+    
+    public Persona(String nombre){
+        this. nombre = nombre;
+        puntuacion = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", puntuacion=" + puntuacion + '}';
+    }
+    
+}
